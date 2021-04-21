eb96fb2a4c0dd10339b704d6a1b36ee2c68cb860
Matt Lu <Matt.Lu@civica.com.au>
2021-04-22 08:32:27 +1000

    Improve Roman to Integer

--- src/roman-to-integer/roman-to-integer.js
+++ src/roman-to-integer/roman-to-integer.js
@@ -1,38 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
 var romanToInt = function (s) {
   const map = new Map();
   map.set("I", 1);
-  map.set("IV", 4);
   map.set("V", 5);
-  map.set("IX", 9);
   map.set("X", 10);
-  map.set("XL", 40);
   map.set("L", 50);
-  map.set("XC", 90);
   map.set("C", 100);
-  map.set("CD", 400);
   map.set("D", 500);
-  map.set("CD", 400);
-  map.set("CM", 900);
   map.set("M", 1000);
 
   let n = 0;
   for (let i = 0; i < s.length; i++) {
-    let r = "";
-    if (i < s.length - 1) {
-      r = s.charAt(i).concat(s.charAt(i + 1));
-      if (map.get(r) !== undefined) {
-        n += map.get(r);
-        i++;
-      } else {
-        r = s.charAt(i);
-        n += map.get(r);
-      }
+    const curr = map.get(s[i]);
+    const next = map.get(s[i + 1]) === undefined ? 0 : map.get(s[i + 1]);
+    if (curr < next) {
+      n -= curr;
     } else {
-      r = s.charAt(i);
-      n += map.get(r);
+      n += curr;
     }
   }
-
   return n;
 };
 
